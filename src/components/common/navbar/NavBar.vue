8800292d628dd6d31b7d3c78941a0384f79de388
caomaozun <595664727@qq.com>
2021-04-25 23:12:05 +0800

    '0425'

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="nav-bar">
+    <!-- div把slot包起来可以设置样式 -->
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NavBar"
+  }
+</script>
+
+<style scoped>
+  .nav-bar {
+    /*css3flex弹性布局*/
+    display: flex;
+    /*height: 44px;*/
+    line-height: 44px;
+    /*text-align文本对齐*/
+    text-align: center;
+    /*css3边框阴影*/
+    box-shadow: 0 1px 1px rgba(100,100,100,.1);
+  }
+  .left, .right {
+    width: 60px;
+  }
+  .center {
+    /*flex-grow: number*/
+    flex: 1;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,50 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from "@/components/common/navbar/NavBar";
+  import {getHomeMultidata} from '@/network/home'
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+        // result: null,
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        // console.log(res);
+        // this.result = res
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  #home {
+    padding-top: 44px;
+  }
+  .home-nav {
+    /*base.css设置的变量*/
+    background-color: var(--color-tint);
+    /*文字颜色*/
+    color: #fff;
+    /*CSS2的BFC中position: fixed*/
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
 </style>
